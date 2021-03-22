37ffc57bc9b9a97fa83f3dc77cc39d6655e77f96
Artem Tokarevskikh <tokarevskikh@gmail.com>
2021-03-22 17:07:16 +0300

    Loaders for styles (css, scss) were added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import {sayHello as greetings} from './greetings'
+import {sayHello as greetings} from "./greetings"
+import lib from "./lib.css"
+import application from "./application.scss"
 
 greetings()
