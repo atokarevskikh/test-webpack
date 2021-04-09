038cffa2948a54ddea120683539db1fc8ddf61ba
Artem Tokarevskikh <tokarevskikh@gmail.com>
2021-04-09 17:16:05 +0300

    Testing branches

--- src/greetings.js
+++ src/greetings.js
@@ -1,7 +1,9 @@
+import {log} from "./console-log"
+
 function sayHello(){
     let tool = "webpack"
     alert(`Привет от ${tool}, welcome to ES6!!!`)
-    console.log("Can you find me?")
+    log("Can you find me?")
 }
 
 export  {sayHello}
